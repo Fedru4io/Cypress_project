2432e372e227affa21b4e825ea0a72137547cf18
Fedru4io <kl1m@rambler.ru>
2026-01-26 17:05:48 +0300

    ENTER

--- cypress/e2e/mytest/FirstTest1.cy.js
+++ cypress/e2e/mytest/FirstTest1.cy.js
@@ -5,13 +5,13 @@ describe('Тесты приложения', () => {
     })
     
     it('Переход на главную', () => {
-      cy.get('[aria-label="На главную"]', { timeout: 10000 })
-        .should('be.visible')
-        .click()
+
         //cy.viewport(1200, 800)
         //cy.get ('rui-tile-menu').find ('div').eq ('1')
       //  cy.contains('Финансово-расчетные операции');
     })
+  
+
   
   })
 
