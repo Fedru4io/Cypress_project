7d1d6a34a4af6c77ea6866d60248e6ad887d0f62
Fedru4io <kl1m@rambler.ru>
2026-01-27 16:31:23 +0300

    оформлен вход

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -25,56 +25,56 @@
 // Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 Cypress.Commands.add('login', () => {
-    // Получаем данные из переменных окружения
-    const username = Cypress.env('username')
-    const password = Cypress.env('password')
+  // Получаем данные из переменных окружения
+  const username = Cypress.env('username')
+  const password = Cypress.env('password')
+
+
+cy.visit('https://by01-vm46.topsoft.local/login')
+
   
-
-  cy.visit('https://by01-vm46.topsoft.local/login')
+  cy.get('[placeholder="Имя пользователя"][class="dx-texteditor-input"]')
+    .type(username)
   
-    
-    cy.get('[placeholder="Имя пользователя"][class="dx-texteditor-input"]')
-      .type(username)
-    
-    cy.get('[placeholder="Пароль"][class="dx-texteditor-input"]')
-      .type(password, { log: false }) // Скрываем пароль в логах
-    
-    cy.get('form').submit()
+  cy.get('[placeholder="Пароль"][class="dx-texteditor-input"]')
+    .type(password, { log: false }) // Скрываем пароль в логах
+  
+  cy.get('form').submit()
 
-    cy.get('.overlay-form', { timeout: 20000 })
-    .should('not.exist') 
+  cy.get('.overlay-form', { timeout: 20000 })
+  .should('not.exist') 
+  
+  // cy.get('[aria-label="ОК"]', { timeout: 10000 })
+  //   .should('be.visible')
+  //   .click()
     
-    // cy.get('[aria-label="ОК"]', { timeout: 10000 })
-    //   .should('be.visible')
-    //   .click()
-      
-          // cy.get('div[class="dx-button-content"]').contains (' Подключиться ', { timeout: 10000 })
-          // .should('be.visible')
-          // .click()
+        // cy.get('div[class="dx-button-content"]').contains (' Подключиться ', { timeout: 10000 })
+        // .should('be.visible')
+        // .click()
 
-   
+ 
 
-          cy.get('body').then(($body) => {
-            // Проверяем, есть ли элемент с aria-label="ОК"
-            if ($body.find('[aria-label="ОК"]').length > 0) {
-              // Если есть - кликаем на него
-              cy.get('[aria-label="ОК"]', { timeout: 20000 })
-                .should('be.visible')
-                .click()
-           
-                cy.get('[aria-label="На главную"]', { timeout: 10000 })
-                .should('be.visible')
-                .click()
-                
-            } else {
-              // Если нет - ищем кнопку "Подключиться"
-              cy.get('div[class="dx-button-content"]')
-                .contains('Подключиться', { timeout: 10000 })
-                .should('be.visible')
-                .click()
-            }
-          })
+        cy.get('body').then(($body) => {
+          // Проверяем, есть ли элемент с aria-label="ОК"
+          if ($body.find('[aria-label="ОК"]').length > 0) {
+            // Если есть - кликаем на него
+            cy.get('[aria-label="ОК"]', { timeout: 20000 })
+              .should('be.visible')
+              .click()
+         
+              cy.get('[aria-label="На главную"]', { timeout: 10000 })
+              .should('be.visible')
+              .click()
+              
+          } else {
+            // Если нет - ищем кнопку "Подключиться"
+            cy.get('div[class="dx-button-content"]')
+              .contains('Подключиться', { timeout: 10000 })
+              .should('be.visible')
+              .click()
+          }
+        })
 
-          cy.get('.overlay-form', { timeout: 20000 })
-          .should('not.exist') 
-  })
+        cy.get('.overlay-form', { timeout: 20000 })
+        .should('not.exist') 
+})
